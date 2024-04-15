55ac893489358051554c0ecf121c4bf0c4c5df49
JohnnyKolek <jankolek0@gmail.com>
2024-04-15 22:31:47 +0200

    authentication works!

--- src/main/java/com/moviematrix/moviematrix/security/service/LogoutService.java
+++ src/main/java/com/moviematrix/moviematrix/security/service/LogoutService.java
@@ -1,14 +1,18 @@
 package com.moviematrix.moviematrix.security.service;
 
+import com.moviematrix.moviematrix.security.token.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LogoutService implements LogoutHandler {
-   // private final TokenRepository tokenRepository;
+    private final TokenRepository tokenRepository;
 
     @Override
     public void logout(
@@ -22,13 +26,13 @@ public class LogoutService implements LogoutHandler {
             return;
         }
         jwt = authHeader.substring(7);
-//        var storedToken = tokenRepository.findByToken(jwt)
-//                .orElse(null);
-//        if (storedToken != null) {
-//            storedToken.setExpired(true);
-//            storedToken.setRevoked(true);
-//            tokenRepository.save(storedToken);
-//            SecurityContextHolder.clearContext();
-//        }
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+            SecurityContextHolder.clearContext();
+        }
     }
 }

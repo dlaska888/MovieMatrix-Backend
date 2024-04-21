5a1ea288e60c5c92b5aeb1cc2dd00aa18f86d84f
Jan Kołek <jankolek0@gmail.com>
2024-04-21 18:33:18 +0200

    simple swagger config

--- src/main/java/com/moviematrix/moviematrix/controller/AuthenticationController.java
+++ src/main/java/com/moviematrix/moviematrix/controller/AuthenticationController.java
@@ -4,6 +4,7 @@ import com.moviematrix.moviematrix.dto.AuthenticationRequest;
 import com.moviematrix.moviematrix.dto.AuthenticationResponse;
 import com.moviematrix.moviematrix.dto.RegisterRequest;
 import com.moviematrix.moviematrix.security.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,8 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @Operation(summary = "Register User",
+            description = "Register user in application. The Response is access token and refresh token")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
